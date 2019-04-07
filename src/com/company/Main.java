ebd0409c00b0873ce95b6b6d7a3bb5c9020c4b1a
AidaiUzak <aidaibaidzhigitova@gmail.com>
2019-04-08 01:45:04 +0600

    Homework _1_1

---
+++ src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        final String VARIABLE1 = "Привет,";
+        String variable2 = "мир!";
+        String variable = VARIABLE1 + variable2;
+        System.out.println(variable);
+        System.out.println(VARIABLE1);
+    }
+}
